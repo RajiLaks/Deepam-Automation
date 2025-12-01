276e064a2799be85a7d51623f783547c45bd65ed
Pradeep <pradeep.e@medyaan.com>
2025-12-01 11:09:40 +0530

    Pharmacist

--- tests/DoctorTests/DoctorLoginTest.spec.js
+++ tests/DoctorTests/DoctorLoginTest.spec.js
@@ -2,14 +2,14 @@ const {test} = require ('@playwright/test');
 const {LoginPage} = require ('../../Pages/BasePage/LoginPage');
 const {ExcelReader} = require ('../../Utils/ExcelReader');
 
-let page;
-let context;
+// let page;
+// let context;
 
 test.describe('Login', async()=>{
     test('TC001 - Login with valid data', async({page})=>{
         const loginpage = new LoginPage(page);
         const excelreader = new ExcelReader();
-        const dataset = await excelreader.readExcel('C:/Users/RajalakshmiRajasekar/Desktop/Deepam_Automation/Deepam/Utils/Deepam_Dataset.xlsx', 'Login');
+        const dataset = await excelreader.readExcel('Utils/Deepam_Dataset.xlsx', 'Login');
         const {URL,MobileNo, Password} = dataset[0];
         await loginpage.LaunchURL(URL);    
         await loginpage.DoctorLogin(MobileNo, Password);
